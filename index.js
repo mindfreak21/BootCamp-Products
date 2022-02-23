52f415e3174f63cbffe425e982d3265d96a42a82
mindfreak21 <rivasm.christopher@gmail.com>
2022-02-22 22:48:24 -0600

    agregue un ejemplo de ejemplo al DOM

---
+++ index.js
@@ -0,0 +1,10 @@
+let paragraphs = document.getElementsByTagName("p");
+console.log(paragraphs);
+
+if (paragraphs.length > 0) 
+{
+    let paragraph = paragraphs[0];
+
+    paragraph.innerText = "Bienvenidos al bootcamp";
+}
+
